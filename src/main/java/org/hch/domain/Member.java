33754412fac2d65c757a6b6c95e71028f581f9ee
hch930 <ruma97@naver.com>
2021-06-03 00:45:06 +0900

    Login

--- src/main/java/org/hch/domain/Member.java
+++ src/main/java/org/hch/domain/Member.java
@@ -1,10 +1,16 @@
 package org.hch.domain;
 
 import java.util.Date;
+import java.util.Set;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinColumns;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 import javax.persistence.Transient;
 import javax.persistence.UniqueConstraint;
@@ -18,10 +24,11 @@ import lombok.Data;
 
 @Data
 @Entity
-@Table(name = "member", uniqueConstraints = {@UniqueConstraint(name = "NAME_ID_UNIQUE", columnNames = {"ID", "EMAIL"})})
+@Table(name = "member", uniqueConstraints = {@UniqueConstraint(name = "ID_EMAIL_UNIQUE", columnNames = {"ID", "EMAIL"})})
 public class Member {
 	
 	@Id
+	@Length(min = 4)
 	private String id;
 	@Column(nullable = false)
 	@NotBlank
@@ -44,5 +51,9 @@ public class Member {
 	private Date regDate;
 	@CreationTimestamp
 	private Date updateDate;
-	
+	@ManyToMany(cascade = CascadeType.MERGE)
+	@JoinTable(name = "member_role",
+			joinColumns = @JoinColumn(name = "member_id", referencedColumnName = "id"),
+			inverseJoinColumns = @JoinColumn(name = "rold_id", referencedColumnName ="id"))
+	private Set<Role> roles;
 }
--- src/main/java/org/hch/domain/Role.java
+++ src/main/java/org/hch/domain/Role.java
@@ -1,9 +1,21 @@
 package org.hch.domain;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.validator.constraints.Length;
+
 import lombok.Data;
 
 @Data
+@Entity
+@Table(name = "roles")
 public class Role {
+	@Id
+	@Length(min = 4)
 	private String id;
+	@Column(unique = true)
 	private String role;
 }
