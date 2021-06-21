40d7970aec5c781a3e39371e728e49ded102bd37
hch930 <ruma97@naver.com>
2021-06-21 17:56:52 +0900

    c

--- src/main/java/org/hch/security/UsstockApplication.java
+++ src/main/java/org/hch/security/UsstockApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Usstock2HchApplication {
+public class UsstockApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Usstock2HchApplication.class, args);
+		SpringApplication.run(UsstockApplication.class, args);
 	}
 
 }
